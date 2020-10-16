08d5349bf21a47b07f05ef5079e91031f9eb46e4
kovacsbalazspeter21 <71692659+kovacsbalazspeter21@users.noreply.github.com>
2020-10-16 08:49:14 +0200

    Add files via upload

---
+++ f0002d.py
@@ -0,0 +1,9 @@
+##F0002d: Rajzold ki egyetlen print() utasítással az alábbi ábrát! (Megoldás itt.) (Ennek a feladatnak – meg a következő kettőnek – az ötlete innen származik.)
+
+print('      /')
+print('     /')
+print('    /')
+print('   /')
+print('  /')
+print(' /')
+print('/')
